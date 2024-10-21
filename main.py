ef996a054470bb225769d878c610b01fc6ec15b5
Eduard Ghenea <edymare97@yahoo.com>
2024-10-21 22:14:54 +0300

    create and get http request to db support added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import psycopg2
+from psycopg2 import sql
 
+scraped_links_table_name = 'scraped_links3'
+to_scrape_links_table_name = 'to_scrap_links'
 
 def scrape_page(url):
     headers = {
@@ -9,7 +13,7 @@ def scrape_page(url):
 
     try:
         # Try to send a request to the URL
-        response = requests.get(url, headers=headers, timeout=10)  # Set a timeout of 10 seconds
+        response = requests.get(url, headers=headers, timeout=10)
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -22,7 +26,7 @@ def scrape_page(url):
             for link in soup.find_all('a', href=True):
                 href = link['href']
                 # Check for both HTTP and HTTPS
-                if href.startswith("http://") or href.startswith("https://") or href.startswith("www"):
+                if (href.startswith("http://") or href.startswith("https://") or href.startswith("www")) and '../' not in href:
                     links.append(href)
                 else:
                     # Convert relative URL to absolute URL if needed
@@ -49,26 +53,76 @@ def scrape_page(url):
         print(f"Unexpected error while scraping {url}: {e}")
         return []
 
+def insert_links(cursor, links, table_name):
+    try:
+        insert_query = (sql.SQL("INSERT INTO {} (link) VALUES (%s) ON CONFLICT (link) DO NOTHING;")
+        .format(sql.Identifier(table_name)))
+        cursor.executemany(insert_query, [(link,) for link in links])
+    except Exception as e:
+        print(f"Error inserting links: {e}")
 
-# Example usage
+def get_links(cursor, table_name):
+    try:
+        select_query = sql.SQL("SELECT link FROM {}").format(sql.Identifier(table_name))
+        cursor.execute(select_query)
+
+        links = cursor.fetchall()
+        return set([link[1] for link in links])
+    except Exception as e:
+        print(f"Error getting links: {e}")
+
+
 if __name__ == "__main__":
+    conn = None
+    cursor = None
+    try:
+        conn = psycopg2.connect(
+            dbname="WebScrapper",
+            user="postgres",
+            password="1234",
+            host="localhost",
+            port="5432"
+        )
+        cursor = conn.cursor()
+        print("Connected to the database.")
 
-    urls_reached = []
-    to_scrape: set[str] = {"http://inf.ucv.ro/"}
-    scraped_links: set[str] = set()
-    while to_scrape:
-        current_url = to_scrape.pop()
-        result = scrape_page(current_url)
+        to_scrape: set[str] = get_links(cursor, to_scrape_links_table_name)
+        #to_scrape.add({'https://about.google/'}) #"http://inf.ucv.ro/"
+        scraped_links: set[str] = set()
+        while to_scrape:
+            current_url = to_scrape.pop()
+            result = scrape_page(current_url)
 
-        for link in result:
-            if link not in urls_reached:
-                to_scrape.add(link)
-            scraped_links.add(link)
+            new_links = set()
+            for link in result:
+                if link not in scraped_links:
+                    to_scrape.add(link)
+                    scraped_links.add(link)
+                    new_links.add(link)
 
-        print('Already reached ' + str(len(urls_reached)))
-        print('To scrape ' + str(len(to_scrape)))
-        print('Scrapped '+ str(len(scraped_links)))
+            if new_links:
+                insert_links(cursor, new_links, scraped_links_table_name)
+                conn.commit()
+                print(f"Inserted {len(new_links)} new links into the database.")
 
-    for idx, link in enumerate(scraped_links):
-        print(f"{idx + 1}. {link}")
-    print("ENDED ------------------")
+                insert_links(cursor, list(to_scrape), to_scrape_links_table_name)
+                conn.commit()
+                print(f"Inserted {len(to_scrape)} links able to be scraped into the database.")
+
+            print('To scrape ' + str(len(to_scrape)))
+            print('Scrapped '+ str(len(scraped_links)))
+
+        for idx, link in enumerate(scraped_links):
+            print(f"{idx + 1}. {link}")
+        print("ENDED ------------------")
+    except psycopg2.Error as e:
+        print(f"Database Error: {e}")
+    except Exception as e:
+        print(f"Error of unknown type: {e}")
+    finally:
+        if cursor is not None:
+            cursor.close()
+            print("Cursor closed.")
+        if conn is not None:
+            conn.close()
+            print("Database connection closed.")
